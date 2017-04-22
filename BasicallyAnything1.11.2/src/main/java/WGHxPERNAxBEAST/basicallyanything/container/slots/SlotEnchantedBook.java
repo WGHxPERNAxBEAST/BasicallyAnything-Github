9390027c2b683e36ea11ef2049a382a0a2d8b058
WGHxPERNAxBEAST <jpwrestle02@gmail.com>
2017-04-22 16:28:26 -0400

    Update-Please check slots in container and recipes

---
+++ BasicallyAnything1.11.2/src/main/java/WGHxPERNAxBEAST/basicallyanything/container/slots/SlotEnchantedBook.java
@@ -0,0 +1,19 @@
+package WGHxPERNAxBEAST.basicallyanything.container.slots;
+
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
+
+public class SlotEnchantedBook extends SlotItemHandler {
+
+	public SlotEnchantedBook(IItemHandler itemHandler, int index, int xPosition, int yPosition) {
+		super(itemHandler, index, xPosition, yPosition);
+	}
+	
+	@Override
+	public boolean isItemValid(ItemStack stack) {
+		return super.isItemValid(stack) && stack.getItem() == Items.ENCHANTED_BOOK;
+	}
+
+}
